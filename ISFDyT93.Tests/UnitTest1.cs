dc2090fcd226d8c2c6ef554504ece535c2fb8e2b
Eliana <valdezeliana38@gmail.com>
2026-04-08 00:40:50 -0300

    test: add unit tests for CrearSlug method

---
+++ ISFDyT93.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ISFDyT93.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ISFDyT93.Tests/ValidacionesTests.cs
@@ -0,0 +1,106 @@
+﻿using Xunit;
+using ISFDyT93.Negocio;
+
+public class ValidacionesTests
+{
+    private readonly Validaciones _validaciones;
+
+    public ValidacionesTests()
+    {
+        _validaciones = new Validaciones();
+    }
+
+    // --- Casos base ---
+
+    [Fact]
+    public void CrearSlug_TextoNormal_DeberiaConvertirCorrectamente()
+    {
+        var result = _validaciones.CrearSlug("Hola Mundo");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoConAcentos_DeberiaQuitarlos()
+    {
+        var result = _validaciones.CrearSlug("Canción Árbol");
+
+        Assert.Equal("cancion-arbol", result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoConMayusculas_DeberiaPasarAMinusculas()
+    {
+        var result = _validaciones.CrearSlug("HOLA MUNDO");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    // --- Caracteres especiales y guiones ---
+
+    [Fact]
+    public void CrearSlug_TextoConCaracteresEspeciales_DeberiaEliminarlos()
+    {
+        var result = _validaciones.CrearSlug("Hola!!! Mundo@@@");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoConMultiplesEspacios_DeberiaUnificarlos()
+    {
+        var result = _validaciones.CrearSlug("Hola    Mundo");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoConMultiplesGuiones_DeberiaReducirlos()
+    {
+        var result = _validaciones.CrearSlug("Hola---Mundo");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoConGuionesAlInicioYFin_DeberiaTrimarlos()
+    {
+        var result = _validaciones.CrearSlug("   Hola Mundo   ");
+
+        Assert.Equal("hola-mundo", result);
+    }
+
+    // --- Edge cases ---
+
+    [Fact]
+    public void CrearSlug_TextoVacio_DeberiaRetornarStringVacio()
+    {
+        var result = _validaciones.CrearSlug("");
+
+        Assert.Equal(string.Empty, result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoNull_DeberiaRetornarStringVacio()
+    {
+        var result = _validaciones.CrearSlug(null);
+
+        Assert.Equal(string.Empty, result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoSoloEspacios_DeberiaRetornarStringVacio()
+    {
+        var result = _validaciones.CrearSlug("   ");
+
+        Assert.Equal(string.Empty, result);
+    }
+
+    [Fact]
+    public void CrearSlug_TextoSoloCaracteresEspeciales_DeberiaRetornarStringVacio()
+    {
+        var result = _validaciones.CrearSlug("!@#$%");
+
+        Assert.Equal(string.Empty, result);
+    }
+}
